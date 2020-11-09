e818e8d45e19e73c7aab3dc2d2f3eb4b3bd09e22
Alexander-Palmer <50350618+Alexander-Palmer@users.noreply.github.com>
2020-11-09 12:25:52 +1000

    Update featureCounts_annotation.R

--- featureCounts_annotation.R
+++ featureCounts_annotation.R
@@ -8,15 +8,9 @@ BiocManager::install("Rsubread")
 library(Rsubread)
 
 #Data input
-proc.files <- c("S996-1-filtered-sorted.bam", "S996-2-filtered-sorted.bam", "S996-3-filtered-sorted.bam",
-           "S997-1-filtered-sorted.bam", "S997-2-filtered-sorted.bam", "S997-3-filtered-sorted.bam",
-           "S998-1-filtered-sorted.bam", "S998-2-filtered-sorted.bam", "S998-3-filtered-sorted.bam",
-           "S999-1-filtered-sorted.bam", "S999-2-filtered-sorted.bam", "S999-3-filtered-sorted.bam",
-           "S1000-1-filtered-sorted.bam", "S1000-2-filtered-sorted.bam", "S1000-3-filtered-sorted.bam",
-           "S1045-1-filtered-sorted.bam", "S1045-2-filtered-sorted.bam", "S1045-3-filtered-sorted.bam",
-           "S1046-1-filtered-sorted.bam", "S1046-2-filtered-sorted.bam", "S1046-3-filtered-sorted.bam",
-           "S1047-1-filtered-sorted.bam", "S1047-2-filtered-sorted.bam", "S1047-3-filtered-sorted.bam")
-
+proc.files <- c("S998-1-filtered-sorted.bam", "S998-2-filtered-sorted.bam", "S998-3-filtered-sorted.bam",
+                "S1000-1-filtered-sorted.bam", "S1000-2-filtered-sorted.bam", "S1000-3-filtered-sorted.bam")
+           
 #featureCounts processing
 meta_results <-featureCounts(files=proc.files,
                        annot.ext="Caenorhabditis_elegans.WBcel235.96.gtf",
