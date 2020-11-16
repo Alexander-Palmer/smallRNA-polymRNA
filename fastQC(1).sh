68cf6e020766dbef7b62a73e0317f1aaa4a417fa
Alexander-Palmer <50350618+Alexander-Palmer@users.noreply.github.com>
2020-11-16 15:45:11 +1000

    Update and rename fastQC_miRNAs.sh to fastQC(1).sh

--- fastQC(1).sh
+++ fastQC(1).sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Perform fastQC analysis on small RNA-seq data prior to and after 
-#adapter trimming
+#cutadapt(1) adapter trimming
 #################################################################
 
-#Before cutadapt adapter clipping
+#Before cutadapt(1) adapter clipping
 fastqc A-284-1_intestine_ALG1.fastq A-284-2_intestine_ALG1.fastq \
        A-285-1_intestine_ALG2.fastq A-285-2_intestine_ALG2.fastq \
        A-286-1_neuron_ALG1.fastq A-286-2_neuron_ALG1.fastq \
@@ -13,7 +13,7 @@ fastqc A-284-1_intestine_ALG1.fastq A-284-2_intestine_ALG1.fastq \
        A-291-1_bwm_ALG2.fastq A-291-2_bwm_ALG2.fastq \
        -o ../FastQC_out -f fastq
 
-#After cutadapt adapter clipping
+#After cutadapt(1) adapter clipping
 fastqc A-284-1_intestine_ALG1_clip.fastq A-284-2_intestine_ALG1_clip.fastq \
        A-285-1_intestine_ALG2_clip.fastq A-285-2_intestine_ALG2_clip.fastq \
        A-286-1_neuron_ALG1_clip.fastq A-286-2_neuron_ALG1_clip.fastq \
