81313416a06280df8c5240a5727e5d2f06334ec0
Alexander-Palmer <50350618+Alexander-Palmer@users.noreply.github.com>
2020-11-16 15:42:33 +1000

    Update fastQC_mRNAs.sh

--- fastQC_mRNAs.sh
+++ fastQC_mRNAs.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-#Perform fastQC analysis on sequencing data before and after 
+#Perform fastQC analysis on sequencing data prior to and after 
 #adapter trimming
-############################################################
+##############################################################
 
+#Before cutadapt adapter trimming
 fastqc S998-1_combined_R1.fastq S998-1_combined_R2.fastq \
+      S998-2_combined_R1.fastq S998-2_combined_R2.fastq \
+      S998-3_combined_R1.fastq S998-3_combined_R2.fastq \
+      S1000-1_combined_R1.fastq S1000-1_combined_R2.fastq \
+      S1000-2_combined_R1.fastq S1000-2_combined_R2.fastq \
+      S1000-3_combined_R1.fastq S1000-3_combined_R2.fastq \
+      -o ../FastQC_out -f fastq
+
+fastqc S998-1_R1.fastq S998-1_combined_R2.fastq \
       S998-2_combined_R1.fastq S998-2_combined_R2.fastq \
       S998-3_combined_R1.fastq S998-3_combined_R2.fastq \
       S1000-1_combined_R1.fastq S1000-1_combined_R2.fastq \
