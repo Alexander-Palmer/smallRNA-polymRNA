f1974edfe66193f7ecdf209e945eb258a0e1ff32
Alexander-Palmer <50350618+Alexander-Palmer@users.noreply.github.com>
2020-11-16 15:43:57 +1000

    Update fastQC_mRNAs.sh

--- fastQC_mRNAs.sh
+++ fastQC_mRNAs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Perform fastQC analysis on sequencing data prior to and after 
-#adapter trimming
+#cutadapt (2) adapter trimming
 ##############################################################
 
-#Before cutadapt adapter trimming
+#Before cutadapt(2) adapter trimming
 fastqc S998-1_combined_R1.fastq S998-1_combined_R2.fastq \
       S998-2_combined_R1.fastq S998-2_combined_R2.fastq \
       S998-3_combined_R1.fastq S998-3_combined_R2.fastq \
@@ -13,10 +13,11 @@ fastqc S998-1_combined_R1.fastq S998-1_combined_R2.fastq \
       S1000-3_combined_R1.fastq S1000-3_combined_R2.fastq \
       -o ../FastQC_out -f fastq
 
-fastqc S998-1_R1.fastq S998-1_combined_R2.fastq \
-      S998-2_combined_R1.fastq S998-2_combined_R2.fastq \
-      S998-3_combined_R1.fastq S998-3_combined_R2.fastq \
-      S1000-1_combined_R1.fastq S1000-1_combined_R2.fastq \
-      S1000-2_combined_R1.fastq S1000-2_combined_R2.fastq \
-      S1000-3_combined_R1.fastq S1000-3_combined_R2.fastq \
+#After cutadapt(2) adapter trimming
+fastqc S998-1_R1.fastq S998-1_R2.fastq \
+      S998-2_R1.fastq S998-2_R2.fastq \
+      S998-3_R1.fastq S998-3_R2.fastq \
+      S1000-1_R1.fastq S1000-1_R2.fastq \
+      S1000-2_R1.fastq S1000-2_R2.fastq \
+      S1000-3_R1.fastq S1000-3_R2.fastq \
       -o ../FastQC_out -f fastq
