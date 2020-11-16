fa91e6826aa774c61d08eb5d4a0ed4b53a1bccc0
Alexander-Palmer <50350618+Alexander-Palmer@users.noreply.github.com>
2020-11-16 15:40:21 +1000

    Update bowtie2.sh

--- bowtie2.sh
+++ bowtie2.sh
@@ -8,10 +8,10 @@ bowtie2-build -f C.elegans.WBcel235.dna.toplevel.fa cel_index
 
 #bowtie2 alignment
 bowtie2 -x cel_index -U \
-A-284-1_intestine_ALG1.fastq,A-284-2_intestine_ALG1.fastq,\
-A-285-1_intestine_ALG2.fastq,A-285-2_intestine_ALG2.fastq,\
-A-286-1_neuron_ALG1.fastq,A-286-2_neuron_ALG1.fastq,\
-A-287-1_neuron_ALG2.fastq,A-287-2_neuron_ALG2.fastq,\
-A-290-1_bwm_ALG1.fastq,A-290-2_bwm_ALG1.fastq,\
-A-291-1_bwm_ALG2.fastq,A-291-2_bwm_ALG2.fastq,\
+A-284-1_intestine_ALG1_clip.fastq,A-284-2_intestine_ALG1_clip.fastq,\
+A-285-1_intestine_ALG2_clip.fastq,A-285-2_intestine_ALG2_clip.fastq,\
+A-286-1_neuron_ALG1_clip.fastq,A-286-2_neuron_ALG1_clip.fastq,\
+A-287-1_neuron_ALG2_clip.fastq,A-287-2_neuron_ALG2_clip.fastq,\
+A-290-1_bwm_ALG1_clip.fastq,A-290-2_bwm_ALG1_clip.fastq,\
+A-291-1_bwm_ALG2_clip.fastq,A-291-2_bwm_ALG2_clip.fastq,\
 -S mapped_seq
