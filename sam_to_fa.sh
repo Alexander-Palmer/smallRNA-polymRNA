1732546421bd0f46d7ec270066be7c6fcc8600bb
Alexander-Palmer <50350618+Alexander-Palmer@users.noreply.github.com>
2020-11-16 17:29:57 +1000

    Delete sam_to_fa.sh

--- sam_to_fa.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Convert bowtie2 .sam output to .fa format for miRDeep2 analysis
-################################################################
-
-for file in *.sam
-do
-  samtools bam2fq $file | seqtk seq -A > $file.fa
